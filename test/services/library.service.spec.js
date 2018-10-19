5329a50e87da08827337c047ea3d0e534b4488c4
farooqam <farooq_am@hotmail.com>
2018-10-18 22:11:54 -0700

    Library service test.

--- test/services/library.service.spec.js
+++ test/services/library.service.spec.js
@@ -1,15 +1,28 @@
 const chai = require("chai");
 const expect = chai.expect;
+const sinon = require("sinon");
+require("sinon-mongoose");
+const MongooseBookModel = require("../../src/schemas/book.schema");
+
 const LibraryService = require("../../src/services/library.service");
 
 describe("Library service", () => {
     describe("Lookup", () => {
-        it("Should get the book", () => {
-            const service = new LibraryService();
+        it("Should get the book from the database", () => {
             const sku = "1234";
+            const title = "Treasure Island";
+
+            const mongooseModelMock = sinon.mock(MongooseBookModel);
+            mongooseModelMock.expects("find").once().withArgs(sku);
+
+            const service = new LibraryService();
             const book = service.lookup(sku);
+
+            mongooseModelMock.verify();
+            mongooseModelMock.restore();
+
             expect(book.sku).to.equal(sku);
-            expect(book.title).to.equal("Moby Dick");
+            expect(book.title).to.equal(title);
         });
     });
 });
